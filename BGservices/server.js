fe3505ec3e8d1818532ffa6786a321b92487c37a
Calvince Ndemo <calyndemo16@gmail.com>
2023-08-05 20:06:29 +0300

    second background service added at the BGservices folder. This enables the admin to receive email when a client completes a certain project

--- BGservices/server.js
+++ BGservices/server.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const cron = require('node-cron');
 const { assigned } = require('./emailService/userAssigned');
+const { completed } = require('./emailService/adminEmail');
 
 const app = express()
 
 cron.schedule('*/5 * * * * *', async()=>{
     console.log('running a task after 5 seconds');
     await assigned()
+    await completed()
 })
 
 app.listen(5001,()=>{
