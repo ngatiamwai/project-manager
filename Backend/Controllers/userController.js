f15092fd256246adb7295bfe1c5d180d2f2a7ef7
ngatiamwai <ngatiamwai25@gmail.com>
2023-08-09 23:33:57 +0300

    test file for verifytoken

--- Backend/Controllers/userController.js
+++ Backend/Controllers/userController.js
@@ -2,6 +2,8 @@ const {v4}=require('uuid')
 const mssql=require('mssql')
 const bcrypt=require('bcrypt')
 const jwt=require('jsonwebtoken')
+const body_parser = require('body-parser')
+const express = require('express')
 
 const { sqlConfig } = require('../Config/config')
 const { createProjectsTable, createTableUser } = require('../Database/Tables/createTables')
@@ -14,17 +16,13 @@ const registerUser=async(req,res)=>{
     try {
         createTableUser()
         const userId=v4()
-
         const {userName,userPhone,userEmail,userPassword,role}=req.body
-
         const {error}=userRegisterValidator.validate(req.body)
         if(error){
             return res.status(422).json(error.details[0].message)
         }
         const salt = await bcrypt.genSalt(10)
         const hashedPassword=await bcrypt.hash(userPassword, salt)
-
-
         const pool = await mssql.connect(sqlConfig)
         if(pool.connected){
             const result = (await pool.request()
@@ -37,16 +35,12 @@ const registerUser=async(req,res)=>{
             .execute('registerUserProc'))
             if(result.rowsAffected[0]==1){
                 return res.status(200).json({message:"User Registered Succesful"})
-
             }else{
-
                 return res.status(400).json({message:"Error Registering User"})
             }
         }else{
             return res.json({message: 'pool not connected'})
         }   
-        
-
     } catch (error) {
        
        return res.json({Error: error.message})
