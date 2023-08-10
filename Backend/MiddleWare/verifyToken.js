a18775f2783722187390a89d185c95948a3da4e9
ngatiamwai <ngatiamwai25@gmail.com>
2023-08-10 15:19:00 +0300

    Edited VerifyToken.spec.js

--- Backend/MiddleWare/verifyToken.js
+++ Backend/MiddleWare/verifyToken.js
@@ -8,14 +8,16 @@ module.exports.tokenVerfying = async(req,res,next)=>{
         const token = req.headers['token']
         if(!token){
             return res.status(401).json({message:'You are not allowed to be seeing This, provide a token'})
+        }else{
+            const decodedData=jwt.verify(token,process.env.SECRET)
+            req.info = decodedData
+            next()
         }
-        const decodedData=jwt.verify(token,process.env.SECRET)
-        req.info = decodedData
         
     } catch (error) {
         res.json({Error: error.message})
     }
-    next()
+    
 }
 
 // module.exports={
