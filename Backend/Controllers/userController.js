4a898332fb30983a709694234bca688eea9e5068
David Munyiri <davidmumyiri2019@gmail.com>
2023-08-13 10:40:50 +0300

    refactored code on the backend in the allusers controller to make the code meet the needs of its use, made changes in the frontend in the admin js file to enhnce the functionalities of the admin

--- Backend/Controllers/userController.js
+++ Backend/Controllers/userController.js
@@ -52,7 +52,7 @@ const allusers = async(req,res)=>{
 try {
     const pool=await mssql.connect(sqlConfig)
     const users = (await pool.request().execute('getAllUsersProc')).recordset
-    res.status(200).json({message:"Here is the list of users",users:allusers})
+    res.status(200).json({message:"Here is the list of users",users})
 } catch (error) {
     return res.json({error})
 }
@@ -61,7 +61,7 @@ try {
 //USER LOGIN Controller
 const loginUser=async(req,res)=>{
     try {
-        const {userName,userPassword}=req.body 
+        const {userName,userPassword,role}=req.body 
 
         if(!userName || !userPassword){
             return res.status(400).json({error:"Kindly input your credentials"})
@@ -81,7 +81,7 @@ const loginUser=async(req,res)=>{
         if(comparePassword){
             const {userPassword,role,userId,...payload}=user 
             const token=jwt.sign(payload, process.env.SECRET,{expiresIn:'360000s'})
-            return res.status(200).json({message:'Logged in successful',token:token})
+            return res.status(200).json({message:'Logged in successful',token:token,role})
                 
            }else{
             return res.status(400).json({message:'Invalid Log in'})
@@ -185,7 +185,7 @@ const viewAllAssignedProjects=async(req,res)=>{
         const {assigned}=req.params
         const pool=await mssql.connect(sqlConfig)
         
-        const projectsAssigned=(await pool.request().input('assigned',assigned).execute('viewAllAssignedProjectsProc')).recordset
+        const projectsAssigned=(await pool.request().input('assigned',assigned).execute('viewAllAssignedProjectsProc')).recordsets
 
         return res.json({projects:projectsAssigned})
         
